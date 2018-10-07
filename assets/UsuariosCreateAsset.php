fe553c1c7921337e7900c19bccbb2c4545061db0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-10-07 02:54:36 +0200

    Creo asset para la vista de alta de usuarios

---
+++ assets/UsuariosCreateAsset.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @author Raúl Caro Pastorino
+ * @link https://fryntiz.es
+ * @copyright Copyright (c) 2018 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ **/
+
+namespace app\assets;
+
+use yii\web\AssetBundle;
+
+/**
+ * Assets para vistas de Usuarios
+ */
+class UsuariosCreateAsset extends AssetBundle
+{
+    public $basePath = '@webroot';
+    public $baseUrl = '@web';
+    public $css = [
+        'css/usuarios/create.css',
+    ];
+    public $js = [
+        'js/usuarios/create.js',
+    ];
+    public $depends = [
+
+    ];
+}
