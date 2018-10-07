4623acb0d1b4507f50cc070425b11b42d64071ea
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-10-07 02:56:06 +0200

    Registro assets

--- views/usuarios/create.php
+++ views/usuarios/create.php
@@ -1,14 +1,26 @@
 <?php
 
+/**
+ * @author Raúl Caro Pastorino
+ * @link https://fryntiz.es
+ * @copyright Copyright (c) 2018 Raúl Caro Pastorino
+ * @license https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ **/
+
+use app\assets\UsuariosCreateAsset;
 use yii\helpers\Html;
 
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Usuarios */
 
-$this->title = 'Create Usuarios';
+$this->title = 'Crear nuevo usuario';
 $this->params['breadcrumbs'][] = ['label' => 'Usuarios', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
+
+// Registro assets para esta vista
+UsuariosCreateAsset::register($this);
+
 ?>
 <div class="usuarios-create">
 
