1763e1420c086028ae0038f2728a86ee53d87057
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-10-09 21:53:26 +0200

    Aviso en página principal

--- views/site/index.php
+++ views/site/index.php
@@ -20,48 +20,18 @@ use \app\helpers\translations;
 
 ?>
 <div class="site-index">
+    <h1>Torrent Libre</h1>
+    <h2>Sitio temporal con el desarrollo</h2>
+    <p>
+        Esta aplicación web está en desarrollo y es mi proyecto
+        integrado para final de DAW.
+    </p>
 
-    <div class="jumbotron">
-        <h1>Congratulations!</h1>
+    <p>
+        Los datos introducidos no son reales y se perderán en cualquier momento.
+    </p>
 
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="http://www.yiiframework.com">Get started with Yii</a></p>
-    </div>
-
-    <div class="body-content">
-
-        <div class="row">
-            <div class="col-lg-4">
-                <h2>Test1</h2>
-
-                <p>
-                    <?= \Yii::t('main', 'contact') ?>
-                </p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-default" href="http://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
-    </div>
+    <p class="alert-warning">
+        No uses esta aplicación hasta que entre en una fase beta más avanzada.
+    </p>
 </div>
