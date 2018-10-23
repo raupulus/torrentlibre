2d2c8ec8097bb1774b349e7beeb5339fc6c5505b
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-10-23 23:52:51 +0200

    Agrego política de privacidad

---
+++ views/site/politicaprivacidad.php
@@ -0,0 +1,291 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fryntiz
+ * Date: 23/10/18
+ * Time: 15:05
+ */
+/**
+ * @author    Raúl Caro Pastorino
+ * @link      https://fryntiz.es
+ * @copyright Copyright (c) 2018 Raúl Caro Pastorino
+ * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ **/
+
+/* @var $this yii\web\View */
+
+$this->title = 'Aviso Legal';
+
+?>
+<div class="site-avisolegal container">
+    <h1>Políticas de Privacidad en <span>TorrentLibre</span></h1>
+
+    <section class="row col-sm-12">
+        <h2>POLÍTICA DE PRIVACIDAD</h2>
+        <div>
+            <p>
+                Hasta ahora se venía cumpliendo la LOPD para temas de privacidad de datos, ley que a partir del 25 de Mayo del 2018 queda sustituida por el Reglamento General de Protección de datos (RGPD) de la Unión Europea.
+            </p>
+
+            <p>
+                La presente Política de Privacidad será válida únicamente para los datos de carácter personal obtenidos en el Sitio Web, no siendo aplicable para aquella información recabada por terceros en otras páginas web, incluso si éstas se encuentran enlazadas por el Sitio Web.
+            </p>
+
+            <p>
+                Con ello este Sitio Web manifiesta su compromiso por mantener y garantizar las relaciones comerciales con el Usuario de forma segura mediante la protección de sus datos personales y garantizando su derecho a la privacidad.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Datos Personales</h2>
+        <div>
+            <p>
+                Datos personales que requiere en el Sitio Web
+                Para poder prestar nuestros servicios necesitamos algunos datos identificativos según el servicio que Sitio Web te preste:
+            </p>
+        </div>
+
+        <div>
+            <h3>Para los comentarios</h3>
+            <ul>
+                <li>
+                    Nombre → para identificar al Usuario.
+                </li>
+
+                <li>
+                    Email → para conocer el email desde el que se ha hecho el
+                    comentario.
+                </li>
+            </ul>
+        </div>
+
+        <div>
+            <h3>Para el área privada de clientes</h3>
+            <p>
+                El área de clientes se trata de una zona privada. El responsable del Sitio Web te proporcionará un usuario y contraseña para que como Usuario puedas acceder a este servicio restringido, entendiendo que aceptas todas las condiciones legales.
+            </p>
+        </div>
+
+        <div>
+            <h3>Para el formulario de contacto</h3>
+            <ul>
+                <li>Nombre y apellidos → para poder responder a la consulta.</li>
+                <li>Email → para poder responder a la consulta.</li>
+            </ul>
+        </div>
+
+        <div>
+            <p>
+                Adicionalmente, cuando visitas nuestro Sitio Web determinada información se almacena automáticamente por motivos técnicos como la dirección IP asignada por tu proveedor de acceso a Internet.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Legitimación</h2>
+        <div>
+            <p>
+                Gracias al consentimiento podemos tratar tus datos, siendo este un requisito obligatorio para que puedas acceder de forma correcta a los servicios del Sitio Web.
+            </p>
+
+            <p>
+                El usuario dispone de su derecho al olvido cuando quiera que todos sus datos sean eliminados de nuestras base de datos.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Categoría de los datos</h2>
+        <div>
+            <p>
+                Los datos que recopila Sitio Web no son considerados datos sensibles o protegidos.
+            </p>
+            
+            <p>
+                Según el artículo 9.1 del nuevo reglamento general de protección de datos de la unión europea (RGPD) se consideran datos sensibles o protegidos los datos personales que revelen:
+            </p>
+
+            <ul>
+                <li>El origen étnico o racial.</li>
+                <li>Las opiniones políticas.</li>
+                <li>Las convicciones religiosas o filosóficas.</li>
+                <li>La afiliación sindical.</li>
+                <li>El tratamiento de datos genéticos.</li>
+                <li>Datos biométricos dirigidos a identificar de manera unívoca a una persona física.</li>
+                <li>Datos relativos a la salud.</li>
+                <li>Datos relativos a la vida sexual o las orientación sexuales de una persona física.</li>
+            </ul>
+
+            <p>
+                Sólo trabajamos con los datos mínimos identificativos para llevar a cabo nuestra labor: nombre, apellidos, nick y email.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Tiempo de conservación de los datos</h2>
+        <div>
+            <p>
+                Los datos del cliente: nombre, correo electrónico, etc. permanecerán en nuestro sistema lo que legalmente y técnicamente se requiera para la operativa normal del Sitio Web, a menos que el Usuario ejerza su derecho a su eliminación o actualización.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Normativa de aplicación</h2>
+        <div>
+            <p>
+                Desde el el 25 de mayo de 2018, el Sitio Web se rige por el Reglamento General sobre Protección de Datos (RGPD) de la Unión Europea.
+
+                <br />
+
+                <a href="https://www.boe.es/doue/2016/119/L00001-00088.pdf"
+                   alt="Reglamento General sobre Protección de Datos (RGPD)">
+                    https://www.boe.es/doue/2016/119/L00001-00088.pdf
+                </a>
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Medidas de seguridad</h2>
+        <div>
+            <p>
+                Este Sitio Web está alojado en los sistemas operados por Heroku (Desarrollo), Acens y Aruba.it para producción, en adelante Servidores, están implantadas las medidas de seguridad técnicas y organizativas necesarias para garantizar la seguridad de los datos de carácter personal del Usuario y evitar así su alteración, pérdida y tratamiento y/o acceso no autorizado. Esto habida cuenta del estado de la tecnología, la naturaleza de los datos almacenados y los riesgos a que están expuestos, ya provengan de la acción humana o del medio físico o natural. Todo ello de conformidad con lo previsto en el RGPD.
+            </p>
+
+            <p>
+                Asimismo, en los Servidores se han establecido medidas adicionales en orden a reforzar la confidencialidad e integridad de la información en este Sitio Web. Manteniendo continuamente la supervisión, control y evaluación de los procesos para asegurar el respeto a la privacidad de los datos.
+            </p>
+
+            <p>
+                Concretamente, las medidas que se adoptan en las empresas de hosting/vps son:
+            </p>
+
+            <ul>
+                <li>Conexión con certificado SSL en el Sitio Web.</li>
+                <li>Encriptación de las claves y contraseñas utilizando algoritmos criptográficos estándar.</li>
+                <li>Protección de los servidores mediante cortafuegos, aislamiento de procesos, y otras medidas de seguridad estándar.</li>
+                <li>Copias de seguridad periódicas distribuidas geográficamente para garantizar la continuidad del servicio en caso de incidente.</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Ejercicios de derechos</h2>
+        <div>
+            <p>
+                Aquel Usuario que haya facilitado sus datos al Sitio Web, podrá dirigirse al titular de la web con el fin de poder ejercitar gratuitamente su derecho de acceso a sus datos, rectificación o supresión, limitación y oposición respecto de los datos incorporados en sus ficheros.
+            </p>
+
+            <p>
+                El método que debe utilizar el Usuario para comunicarse con el Sitio Web será mediante su cuenta de email registrada en nuestro servicio web, o por escrito.
+            </p>
+
+            <p>
+                El Usuario podrá ejercitar sus derechos mediante comunicación
+                por escrito dirigida al Sitio Web con la referencia
+                "Protección de datos", especificando sus datos, acreditando
+                su identidad y los motivos de su solicitud en la siguiente
+                dirección:
+                <a href="mailto:webtorrentlibre@gmail.com"
+                   alt="webtorrentlibre@gmail.com">
+                    webtorrentlibre@gmail.com
+                </a>
+            </p>
+
+            <p>
+                Cualquier información que necesitemos almacenar en virtud de una obligación legal, fiscal o contractual, será bloqueada y sólo utilizada para dichos fines en lugar de ser borrada.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Modificación de la política de privacidad</h2>
+        <div>
+            <p>
+                El Sitio Web se reserva el derecho de modificar la Política de Privacidad, motivado por un cambio legislativo, jurisprudencial o doctrinal de la Agencia Española de Protección de Datos.
+            </p>
+
+            <p>
+                Cualquier modificación de la Política de Privacidad será publicada al menos diez días antes de su efectiva aplicación.
+            </p>
+
+            <p>
+                Responsable del fichero y encargado del tratamiento
+                El responsable del fichero de datos es el titular del Sitio Web.
+            </p>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Servicios ofrecidos por terceros</h2>
+        <div>
+            <p>
+                Para prestar servicios estrictamente necesarios para su adecuado funcionamiento Sitio Web utiliza los siguientes prestadores de servicios bajo sus correspondientes condiciones de privacidad:
+            </p>
+
+            <ul>
+                <li>Google Analytics, para estadísticas. https://policies.google.com/privacy?hl=es</li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Plugins sociales</h2>
+        <div>
+            <p>
+                En nuestro Sitio Web ofrecemos enlaces y servicios relacionados con diferentes redes sociales. Si el Usuario es miembro de una red social y hace clic sobre el correspondiente enlace, el proveedor de la red social podrá enlazar sus datos de perfil con la información de su visita al Sitio Web.
+            </p>
+
+            <p>
+                Por tanto, el Usuario debe estar informado sobre las funciones y políticas sobre el tratamiento de sus datos personales de la respectiva red social. Tanto si accede a una de nuestras páginas
+            </p>
+
+            <p>
+                El Usuario puedes acceder en todo momento a las políticas de privacidad de las diferentes redes sociales, así como configurar su perfil para garantizar su privacidad.
+            </p>
+
+            <ul>
+                <li>
+                    Facebook:
+                    <a href="https://www.facebook.com/help/323540651073243/">
+                        https://www.facebook.com/help/323540651073243/
+                    </a>
+                </li>
+                <li>
+                    Twitter:
+                    <a href="https://twitter.com/privacy">
+                        https://twitter.com/privacy
+                    </a>
+                </li>
+                <li>
+                    Youtube:
+                    <a href="https://policies.google.com/privacy?hl=es&gl=es">
+                        https://policies.google.com/privacy?hl=es&gl=es
+                    </a>
+                </li>
+                <li>
+                    Google Plus:
+                    <a href="https://policies.google.com/privacy?hl=es&gl=es">
+                        https://policies.google.com/privacy?hl=es&gl=es
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </section>
+
+    <section class="row col-sm-12">
+        <h2>Aceptación, consentimiento y revocabilidad</h2>
+        <div>
+            <p>
+                El Usuario declara haber sido informado de las presentes condiciones sobre protección de datos de carácter personal, aceptando y consintiendo el tratamiento de los mismos por parte de Sitio Web en la forma y para las finalidades indicadas en el Aviso Legal.
+            </p>
+
+            <p>
+                En cualquier momento el Usuario podrá revocar sus datos, pero sin afectar a acciones ya realizadas en el pasado, es decir sin carácter retroactivo.
+            </p>
+        </div>
+    </section>
+</div>
