cf4f82b6bd438fcbc079c11fa58dbe8a0ca2b2a0
Raúl Caro Pastorino <tecnico@fryntiz.es>
2018-11-02 23:39:11 +0100

    Maqueto vista individual de torrents

--- views/site/redessociales.php
+++ views/site/redessociales.php
@@ -19,22 +19,24 @@ RedesSocialesAsset::register($this);
 
 /* @var $this yii\web\View */
 
-$this->title = 'IP Bloqueada';
+$this->title = 'Redes Sociales';
 
 ?>
-<div class="site-iplocked">
+<div class="site-iplocked container">
     <h1>Redes sociales</h1>
     <h2>Sigue esta web desde tus Redes Sociales favoritas</h2>
 
-    <p>
-        Compartimos periódicamente novedades, recopilaciones de mejores
-        torrents y todas las novedades mediante las siguientes Redes Sociales:
-    </p>
+    <div class="row box-content-white">
+        <p>
+            Compartimos periódicamente novedades, recopilaciones de mejores
+            torrents y todas las novedades mediante las siguientes Redes Sociales:
+        </p>
 
-    <ul id="lista-redes-sociales">
-        <li><a href="#"><i class="fa fa-telegram"></i>https://????</a></li>
-        <li><a href="#"><i class="fa fa-twitter"></i>https://????</a></li>
-        <li><a href="#"><i class="fa fa-google-plus"></i>https://????</a></li>
-        <li><a href="#"><i class="fa fa-facebook"></i>https://????</a></li>
-    </ul>
+        <ul id="lista-redes-sociales">
+            <li><a href="#"><i class="fa fa-telegram"></i>https://????</a></li>
+            <li><a href="#"><i class="fa fa-twitter"></i>https://????</a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i>https://????</a></li>
+            <li><a href="#"><i class="fa fa-facebook"></i>https://????</a></li>
+        </ul>
+    </div>
 </div>
