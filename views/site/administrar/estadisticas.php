adeb94c045b3835c0bc9aa85cfa0e18ecfd7c4d5
Raúl Caro Pastorino <dev@fryntiz.es>
2018-11-19 15:05:27 +0100

    Preparo assets para vista de estadísticas

---
+++ views/site/administrar/estadisticas.php
@@ -0,0 +1,20 @@
+<?php
+use app\assets\SiteEstadisticasAsset;
+
+/**
+ * @author    Raúl Caro Pastorino
+ * @link      https://fryntiz.es
+ * @copyright Copyright (c) 2018 Raúl Caro Pastorino
+ * @license   https://www.gnu.org/licenses/gpl-3.0-standalone.html
+ **/
+
+SiteEstadisticasAsset::register($this);
+
+/* @var $this yii\web\View */
+
+$this->title = 'Estadísticas';
+?>
+
+<div id="site-estadisticas" class="container">
+
+</div>
